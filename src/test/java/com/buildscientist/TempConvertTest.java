02ae84ea2748ec18acc0ba28a96a2a3510165205
youssuf.elkalay <yelkalay@gmail.com>
2015-09-11 22:13:29 -0700

    Updated POM with sl4j/log4j related dependencies Removed @ignore from mockCelciusToFarenheight test case Modified sample CelciusToFarenheight resource files Added log4j property file to configure debug logging to STDOUT

--- src/test/java/com/buildscientist/TempConvertTest.java
+++ src/test/java/com/buildscientist/TempConvertTest.java
@@ -43,7 +43,7 @@ public class TempConvertTest {
 	private final static int WIREMOCK_LISTENER_PORT = WireMockUtils.getAvailablePort();
 	private final static String WIREMOCK_BASE_URI = "http://localhost:" + WIREMOCK_LISTENER_PORT;
 	private List<Request> wiremockRequests = new ArrayList<Request>();
-	private final static Logger logger = LoggerFactory.getLogger(TempConvertTest.class);
+	private final static Logger log = LoggerFactory.getLogger(TempConvertTest.class);
 
 	
 	@Before
@@ -90,11 +90,12 @@ public class TempConvertTest {
 		
 	}
 	
-	@Ignore
 	@Test
-	public void mockCelciusToFarenheight() {
+	public void mockCelciusToFarenheight() throws IOException {
 		String conversionEndpointURI = "/tempconvert.asmx?op=CelsiusToFarenheight";	
-		String xpathQuery = "//soapenv:Envelope | //soapenv:Body | //CelsiusToFahrenheitResponse | //CelsiusToFahrenheitResult";
+		String requestXPathQuery = "//soapenv:Envelope | //soapenv:Body | //CelsiusToFahrenheit | //CelsiusToFahrenheit";
+		String responseEnvelope = IOUtils.toString(getClass().getResourceAsStream("/celsiustofarenheight/sampleresponse.xml"),"UTF-8");
+		String requestEnvelope = IOUtils.toString(getClass().getResourceAsStream("/celsiustofarenheight/samplerequest.xml"),"UTF-8");
 		
 		/**
 		 * Mock SOAP endpoint for a SOAP web service that matches a response body with the following SOAP envelope:
@@ -108,12 +109,14 @@ public class TempConvertTest {
 		 * </soap:Envelope>
 		 **/
 		
-		wiremockRule.stubFor(post(urlEqualTo(conversionEndpointURI)).withRequestBody(
-				matchingXPath(xpathQuery)).willReturn(
-				aResponse().withStatus(HttpStatus.ORDINAL_200_OK).withBody("")));
+		wiremockRule.stubFor(post(urlEqualTo(conversionEndpointURI)).withHeader("Content-Type", equalTo("application/soap+xml")).withRequestBody(
+				matchingXPath(requestXPathQuery)).willReturn(
+				aResponse().withStatus(HttpStatus.ORDINAL_200_OK).withBody(responseEnvelope)).withHeader("Content-Type", equalTo("application/soap+xml")));
 		
+		SoapClient client = SoapClient.builder().endpointUri(WIREMOCK_BASE_URI + conversionEndpointURI).build();
+		client.post(requestEnvelope);
 		
-		wiremockRule.verify(postRequestedFor(urlEqualTo(conversionEndpointURI)).withRequestBody(matchingXPath(xpathQuery)));
+		wiremockRule.verify(postRequestedFor(urlEqualTo(conversionEndpointURI)).withRequestBody(matchingXPath(requestXPathQuery)));
 
 	}
 	
